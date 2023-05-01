2f3dc340269d8eefb3558d68270716e3e623b0a7
Mateo Sa <mateo.sa@etu.univ-lehavre.fr>
2023-05-01 22:13:56 +0200

    Ajout de Frame.java

---
+++ java/Frame.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Color;
+
+public class Frame extends JFrame
+{
+	
+}
