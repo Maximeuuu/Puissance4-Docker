ccdb20ed97650f8a73b1e3a9eaef4dd9f8f01f21
MatKim76 <129748653+MatKim76@users.noreply.github.com>
2023-05-02 16:00:09 +0200

    Update Serveur.java

--- java/Serveur.java
+++ java/Serveur.java
@@ -16,27 +16,11 @@ public class Serveur
 		try
 		{
 			ServerSocket ss = new ServerSocket(port);
-
 			
 			this.toClient = ss.accept();
-
+			
 			this.ctrl.lancerPartie();
-
-			/*int cpt = 0;
-			while ( !this.ctrl.estFini() )
-			{
-				if (cpt % 2 == 0)
-				{
-					this.envoyer();
-				}
-				if (cpt % 2 == 1)
-				{
-					this.ctrl.ajouterPiece( this.recevoir() );
-				}
-				
-				cpt++;
-			}*/
-
+			
 			//toClient.close();
 			
 		}
@@ -47,45 +31,42 @@ public class Serveur
 		
 	}
 
-	public void envoyer() {
-    while ( ctrl.getTour() != 1) {}
-    try {
-        PrintWriter out = new PrintWriter(this.toClient.getOutputStream(), true);
-        out.println(ctrl.getDernierCoup() + "");
-        out.close();
-    } catch (IOException e) {
-    }
+	public void envoyer(int col) 
+	{
+		try {
+			PrintWriter out = new PrintWriter(this.toClient.getOutputStream(), true);
+			out.println( col + "");
+			out.close();
+		}
+		catch (IOException e) {
+	}
 
-    ctrl.resetCoup();
 	}
 
 	
-	public int recevoir() {
-    int pos = -1;
+	public int recevoir() 
+	{
+		int pos = -1;
 
-    try {
-        BufferedReader in = new BufferedReader(new InputStreamReader(this.toClient.getInputStream()));
-        String text = in.readLine();
+		try 
+		{
+			BufferedReader in = new BufferedReader(new InputStreamReader(this.toClient.getInputStream()));
+			String text = in.readLine();
 
-        while (ctrl.getTour() != 0) {}
+			while (text == null) 
+			{
+				text = in.readLine();
+				System.out.println(text);
+			}
+			pos = Integer.parseInt(text);
 
-        while (text != null) {
-            try {
-                pos = Integer.parseInt(text);
-                break;
-            } catch (NumberFormatException e) {
-                System.err.println("Erreur de conversion en entier : " + text);
-            }
-            text = in.readLine();
-        }
-
-        in.close();
-    } catch (IOException e) {
-        System.err.println("Erreur d'E/S : " + e.getMessage());
-    }
-
-    ctrl.resetCoup();
-
-    return pos;
+			in.close();
+		} 
+		catch (IOException e) 
+		{
+			System.err.println("Erreur d'E/S : " + e.getMessage());
+		}
+		System.out.println(pos);
+		return pos;
 	}
 }
