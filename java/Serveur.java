e5cc1f3b89164f7cb6db3d68632177fd4c50b049
MatKim76 <129748653+MatKim76@users.noreply.github.com>
2023-05-02 15:20:26 +0200

    Update Serveur.java

--- java/Serveur.java
+++ java/Serveur.java
@@ -17,95 +17,75 @@ public class Serveur
 		{
 			ServerSocket ss = new ServerSocket(port);
 
-			while (true)
+			
+			this.toClient = ss.accept();
+
+			this.ctrl.lancerPartie();
+
+			/*int cpt = 0;
+			while ( !this.ctrl.estFini() )
 			{
-				this.toClient = ss.accept();
-
-				this.ctrl.lancerPartie();
-
-				int cpt = 0;
-				while ( !this.ctrl.estFini() )
+				if (cpt % 2 == 0)
 				{
-					if (cpt % 2 == 0)
-					{
-						this.envoyer();
-					}
-					if (cpt % 2 == 1)
-					{
-						this.ctrl.ajouterPiece( this.recevoir() );
-					}
+					this.envoyer();
 				}
+				if (cpt % 2 == 1)
+				{
+					this.ctrl.ajouterPiece( this.recevoir() );
+				}
+				
+				cpt++;
+			}*/
 
-				//out.println();
-				//in.readLine();
-
-				toClient.close();
-			}
+			//toClient.close();
+			
 		}
 		catch(Exception e)
 		{
 			System.out.println(e);
 		}
-
+		
 	}
 
+	public void envoyer() {
+    while ( ctrl.getTour() != 1) {}
+    try {
+        PrintWriter out = new PrintWriter(this.toClient.getOutputStream(), true);
+        out.println(ctrl.getDernierCoup() + "");
+        out.close();
+    } catch (IOException e) {
+    }
 
-	public void envoyer ()
-	{
-		while ( this.ctrl.getDernierCoup() == -1 )
-		{
-			
-		}
-
-		try{
-		PrintWriter out = new PrintWriter(this.toClient.getOutputStream(), true);
-		out.println ( this.ctrl.getDernierCoup() + "" );
-		out.close();
-		}catch( IOException e ){}
-
-		this.ctrl.resetCoup();
+    ctrl.resetCoup();
 	}
 
-	/*public int recevoir()
-	{
-		int pos = -1;
-		
-		BufferedReader in = new BufferedReader( new InputStreamReader(this.toClient.getInputStream()));
-		while ( pos == -1 )
-		{
-			String text = in.readLine();
-			pos = Integer.parseInt( text );
-		}
-		in.close();
-		
-		
-		return pos;
-	}*/
+	
 	public int recevoir() {
     int pos = -1;
-    
+
     try {
         BufferedReader in = new BufferedReader(new InputStreamReader(this.toClient.getInputStream()));
-        String text = in.readLine(); // Lecture d'une ligne du flux d'entrée
-        
-        // Utilisation d'une boucle pour gérer les lectures multiples
+        String text = in.readLine();
+
+        while (ctrl.getTour() != 0) {}
+
         while (text != null) {
             try {
-                pos = Integer.parseInt(text); // Conversion en entier
-                break; // Sortie de la boucle si la conversion a réussi
+                pos = Integer.parseInt(text);
+                break;
             } catch (NumberFormatException e) {
-                // Gestion de l'exception si la chaîne ne peut pas être convertie en entier
                 System.err.println("Erreur de conversion en entier : " + text);
             }
-            text = in.readLine(); // Lecture de la ligne suivante
+            text = in.readLine();
         }
-        
-        in.close(); // Fermeture du flux d'entrée
+
+        in.close();
     } catch (IOException e) {
-        // Gestion de l'exception d'E/S
         System.err.println("Erreur d'E/S : " + e.getMessage());
     }
-    
+
+    ctrl.resetCoup();
+
     return pos;
 	}
 }
